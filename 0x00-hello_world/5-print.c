2a5c28a73eb2c220a7c078aef8cb6df6f789bf47
Josphat <josphatmaphololo707@gmail.com>
2023-12-17 04:06:48 -0800

    Task 5

--- 0x00-hello_world/5-print.c
+++ 0x00-hello_world/5-print.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return 0;
 }
+
+// Adding a comment to avoid empty translation unit error
