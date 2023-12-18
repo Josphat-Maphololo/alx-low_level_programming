8ce14f8bc707cd4417c7d33a37932b296ed54b56
Josphat <josphatmaphololo707@gmail.com>
2023-12-18 04:58:35 -0800

    task 1

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main A program to select random positve or neagative numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int n;
+srand(time(NULL));
+n = rand() % 201 - 100;
+printf("The number %d is", n);    
+if (n > 0)
+{
+printf("positive");
+}
+else if (n == 0)
+{
+printf("zero");
+}
+else
+{
+printf("negative");
+}
+printf("\n");
+return (0);
+}
