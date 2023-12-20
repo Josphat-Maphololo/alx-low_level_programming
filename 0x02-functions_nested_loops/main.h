23cc1a0611551828bec0789a1d9463f8112e18e0
Josphat <josphatmaphololo707@gmail.com>
2023-12-20 01:56:33 -0800

    TASK 0

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_HEARDER_FILE
+#define MAIN_HEADER_FILE
+int _putcher(char c);
+void print_alphabetic(void);
+void print_alphabetic_x10(void);
+int isl0wer(int c);
+int isalpha(int c);
+int print_sign(int);
+int abs(int);
+int print_last_digit(int);
+void jack_hammer(void);
+void time_table(void);
+int add(int, int);
+void print_to_90(int n);
+#endif
