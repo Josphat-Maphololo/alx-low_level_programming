e71cc1cafeede660db8946490f3c961e3384b516
Josphat <josphatmaphololo707@gmail.com>
2024-01-10 00:07:46 -0800

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
   * _puts_recursion - main function
   *
@@ -8,11 +8,11 @@
   */
 void _puts_recursion(char *s)
 {
-if (*s ==  '0')
+if (*s)
 {
-putchar('\n');
-return;
-}
-putchar(*s);
+_putchar(*s);
 _puts_recursion(s + 1);
+}
+else
+_putchar('\n');
 }
