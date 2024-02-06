5fd612ef0ff51d875815c15055db7e12b76964b4
Josphat <josphatmaphololo707@gmail.com>
2024-02-06 00:19:30 -0800

    Write a function that returns the number of elements in a linked listint_t list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked lists
+ * @pinkie: linked list of type listint_t to travers
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *pinkie)
+{ size_t number = 0;
+while (pinkie)
+{
+number++;
+h = h->next;
+} return (num);
+}
