85b24249e33b11e8c4ab0af3e9da12c868d9f245
Josphat <josphatmaphololo707@gmail.com>
2024-02-06 01:35:07 -0800

    Write a function that returns the number of elements in a linked listint_t list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,19 +2,19 @@
 
 /**
  * add_nodeint - adds a new node at the beginning of a linked list
- * @hobo: pointer to the first node in the list
- * @p: data to insert in that new node
+ * @head: pointer to the first node in the list
+ * @n: data to insert in that new node
  *
  * Return: pointer to the new node, or NULL if it fails
  */
-listint_t*add_nodeint(listint_t**hobo, const int p)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-listint_t *newe;
-newe = malloc(sizeof(listint_t));
-if (!newe)
+listint_t *new;
+new = malloc(sizeof(listint_t));
+if (!new)
 return (NULL);
-newe->p = p;
-newe->next = *hobo;
-*hobo = newe;
-return (newe);
+new->n = n;
+new->next = *head;
+*head = new;
+return (new);
 }
